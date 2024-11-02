f00faadfb972fb30cd73dbf35e3b62ccb26ba763
Fernando Rodrigues <alpha@sigmasquadron.net>
2024-11-02 15:37:44 +0000

    tailor: drop
    Unmaintained package that no longer builds; last upstream update was
    three years ago.
    
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32497,8 +32497,6 @@ with pkgs;
 
   tahoe-lafs = callPackage ../tools/networking/p2p/tahoe-lafs { };
 
-  tailor = callPackage ../applications/version-management/tailor { };
-
   tailor-gui = callPackage ../os-specific/linux/tailor-gui { };
 
   taizen = callPackage ../applications/misc/taizen { };
